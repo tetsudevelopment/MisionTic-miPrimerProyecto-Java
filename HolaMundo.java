22f56a0d8731dc9e82c78b2ec29d33d7657bec13
tetsudevelopment <bresmacawork@gmail.com>
2022-07-20 16:20:47 -0500

    Primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,42 @@
+
+  /**
+   * HolaMundo
+   */
+  //Función principal
+  public class HolaMundo {
+    public static void main(String[] args) {
+      // Comentario de una sola linea de código
+      /* COmentarios
+       * de varias
+       * lineas d
+       * de 
+       * cogiho
+      */
+      //Mostrar mensaje en consola
+      System.out.println("Hola Mundo");
+      System.out.println("Soy Brayan Programando en Java");
+      System.out.println("Segundo salto");
+      
+      //VARIABLES
+      int entero = 10;
+      double decimal = 10.5;
+      boolean bandera = true;
+      //Reservar un espacio en memoria 
+      String mensaje;
+      //Asignar datos al espacio en memoria
+      mensaje = "Hola Mundo desde una variable tipo String";
+      var dinamica = 5;
+      //los char deben ir en comillas solitarias-comillas simples
+      char caracter = 'b';
+      //Imprimir mensaje
+      System.out.println(mensaje);
+      //OPERACIONES 
+      int n1 = 10;
+      int n2 = 20;
+      int suma = n1 + n2;
+      suma += 10;
+      int multiplicacion = n1 * n2;
+      multiplicacion *= 2;
+
+    }
+  }
